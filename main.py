ffdd6aba4d91b976cdc913aec7f2d9b96acc1b59
Can Sever <cansevver01@gmail.com>
2025-06-17 11:42:54 +0200

    add table from excel file

--- main.py
+++ main.py
@@ -7,8 +7,6 @@ from pathlib import Path
 filepaths = glob.glob("Invocies/*.xlsx")
 
 for filepath in filepaths:
-    df = pd.read_excel(filepath, sheet_name="Sheet 1")
-
     pdf = FPDF(orientation='p', unit='mm', format='A4')
 
     pdf.add_page()
@@ -19,6 +17,33 @@ for filepath in filepaths:
     pdf.cell(w=50, h=8, txt=f"Invoice nr.{invoice_nr}", ln=1)
 
     pdf.set_font(family="Times", size=16, style='B')
-    pdf.cell(w=50, h=8, txt=f"Date{date}")
+    pdf.cell(w=50, h=8, txt=f"Date{date}", ln=1)
+
+    df = pd.read_excel(filepath, sheet_name="Sheet 1")
+    ##Tablodaki basliklari aliyoruz, tablodaki basliklari tek bir kere kullanacagimiz icin
+    # yani basliklar sabit oldugu icin bu islemi for loopun disinda yapiyoruz
+
+    columns = list(df.columns)
+    columns = [item.replace("_", " ").title() for item in columns]
+    pdf.set_font(family="Times", size=8, style='B')
+    pdf.set_text_color(80, 80, 80)
+
+    pdf.cell(w=30, h=8, txt=columns[0], border=1)
+    pdf.cell(w=60, h=8, txt=columns[1], border=1)
+    pdf.cell(w=30, h=8, txt=columns[2], border=1)
+    pdf.cell(w=30, h=8, txt=columns[3], border=1)
+    pdf.cell(w=30, h=8, txt=columns[4], border=1, ln=1)
+
+    ## tablodaki verileri almak icin olusturdugumuz itteration
+    for index, row in df.iterrows():
+        pdf.set_font(family="Times", size=8)
+        pdf.set_text_color(80, 80, 80)
+        ## tablonun ilk icerigi olan product_id'deki verileri
+        # ##kismini aldik simdi tablonun devamini alma vakti
+        pdf.cell(w=30, h=8, txt=str(row["product_id"]), border=1)
+        pdf.cell(w=60, h=8, txt=str(row["product_name"]), border=1)
+        pdf.cell(w=30, h=8, txt=str(row["amount_purchased"]), border=1)
+        pdf.cell(w=30, h=8, txt=str(row["price_per_unit"]), border=1)
+        pdf.cell(w=30, h=8, txt=str(row["total_price"]), border=1, ln=1)
 
     pdf.output(f"PDFs/{invoice_nr}.pdf")
